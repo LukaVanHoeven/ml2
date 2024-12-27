5e08ce9810201eeb80df4ace2ba90ae2c0657252
Skylges <skylges@gmail.com>
2024-12-27 13:22:43 +0100

    observation prints

--- air_hockey_agent/agent_builder.py
+++ air_hockey_agent/agent_builder.py
@@ -17,6 +17,7 @@ import numpy as np
 import mujoco as mujoco
 
 from air_hockey_challenge.framework import AgentBase
+from air_hockey_challenge.framework import AirHockeyChallengeWrapper
 
 
 def build_agent(env_info, **kwargs):
@@ -38,35 +39,83 @@ class moveForwardAgent(AgentBase):
         self.new_start = True
         self.new_position = None
         self.episode = 1
-
+        
+        # Available Environments [3dof, 3dof-hit, 3dof-defend],
+        # [7dof, 7dof-hit, 7dof-defend, 7dof-prepare, tournament] will be released at the beginning of
+        # the stage.
+        self.env = AirHockeyChallengeWrapper("3dof-hit")
+        
+        # Print env name and env dict
+        print(self.env.env_name)
+        for k, v in self.env.env_info.items():
+            print(k, v)
+        
+        # # Get the keys of the available constraint
+        # print(env_info['constraints'].keys())                      
+            
+            
+            
+            
+        
+        # instantiate dict (only for readability/ease of use. Could've just used self.env.env_info["robot"]["joint_pos_limit"])
+        self.robot_dict = self.env.env_info["robot"] 
+        self.joint_pos_limit = self.robot_dict["joint_pos_limit"]
+        
+        self.lower_joint_pos_limit = self.robot_dict["joint_pos_limit"][0]
+        self.upper_joint_pos_limit = self.robot_dict["joint_pos_limit"][1]
+        # print(self.lower_joint_pos_limit) # [-2.96705973 -1.8        -2.0943951 ]
 
     def reset(self):
         self.new_start = True
         self.new_position = None
 
     def draw_action(self, observation):
+                
+                
+        
+        # joint_pos_limit = self.env.env_info["joint_pos_limit"]
+        # print(joint_pos_limit)
+        
         
         if self.new_start:
             self.new_start = False
             
-            if self.episode > 0:
-                self.new_position = np.array([-1.15570723, 1.40024401, 1.44280414])
+            
+            # Get the joint position and velocity from the observation
+            q = observation[self.env_info['joint_pos_ids']]
+            dq = observation[self.env_info['joint_vel_ids']]
+            
+            print("AAAAAAAAAAAA")
+            print(q)
+            print(dq)
+            
+            # Get a dictionary of the constraint functions {"constraint_name": ndarray}
+            c = self.env_info['constraints'].fun(q, dq)
+            print(c)
+            
+            # TODO: Why is there a difference between pos_limit and pos_constraint? And how to properly read x,y value coordinates???
+            
+            if self.episode > 1:
+                self.new_position = self.lower_joint_pos_limit
+                # self.new_position = np.array([-1.15570723, 1.30024401, 1.44280414]) # Start position of the robot
             else:
-                self.new_position = self.get_joint_pos(observation)
+                self.new_position = self.upper_joint_pos_limit
+                # self.new_position = self.get_joint_pos(observation)
                 
             self.episode += 1
             
             
-            print("\n newPos", self.new_position)
-            print("Joint pos", self.get_joint_pos(observation))
-            print("Joint vel", self.get_joint_vel(observation))
-            print("Puck pos", self.get_puck_pos(observation))
-            print("Puck vel", self.get_puck_vel(observation))
-            print("Puck state", self.get_puck_state(observation))
-            print("ee pos", self.get_ee_pose(observation))
             
+            # print("\n newPos", self.new_position)
+            # print("Joint pos", self.get_joint_pos(observation))
+            # print("Joint vel", self.get_joint_vel(observation))
+            # print("Puck pos", self.get_puck_pos(observation))
+            # print("Puck vel", self.get_puck_vel(observation))
+            # print("Puck state", self.get_puck_state(observation))
+            # print("ee pos", self.get_ee_pose(observation))
+            # print("obs", observation)
             
-            print(observation)
+            # print("env_info", self.env_info)
 
 
         velocity = np.zeros_like(self.new_position)
